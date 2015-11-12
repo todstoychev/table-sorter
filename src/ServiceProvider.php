e869eceacf9c1f2a4eec0c16356d12aaa933e086
todstoychev <todstoychev@gmail.com>
2015-11-12 09:07:55 +0200

    Created basic classe, service provider and necessary templates

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,31 +1,29 @@
 <?php
 
-namespace Todstoychev\Icr;
+namespace Todstoychev\TableSorter;
 
-use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
-use Todstoychev\Icr\Console\RebuildCommand;
-use Todstoychev\Icr\Handler;
-use Todstoychev\Icr\Reader\DirectoryTreeReader;
 
 /**
  * Service provider class
  *
  * @author Todor Todorov <todstoychev@gmail.com>
- * @package Todstoychev\Icr
+ * @package Todstoychev\TableSorter
  */
 class ServiceProvider extends BaseServiceProvider
 {
     /**
-     * Bootstrap the application services.
+     * @inheritdoc
      */
     public function boot()
     {
-        $this->mergeConfigFrom(__DIR__ . '/config/config.php', 'icr');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'table-sorter');
 
-        $this->publishes([
-            __DIR__ . '/config/config.php' => config_path('icr/config.php'),
-        ]);
+        $this->publishes(
+            [
+                __DIR__ . '/resources/views' => base_path('resources/views/todstoychev/table-sorter'),
+            ]
+        );
     }
 
     /**
@@ -33,79 +31,6 @@ class ServiceProvider extends BaseServiceProvider
      */
     public function register()
     {
-        // Bind ICR configuration
-        $this->app->bind('icr.config', function () {
-            return 'icr.config';
-        });
 
-        // Configuration validator
-        $this->app->bind('icr.configuration_validation.handler', function () {
-            return new Handler\ConfigurationValidationHandler(
-                $this->app->make('icr.config')
-            );
-        });
-
-        // Directory handler
-        $this->app->bind('icr.directory.handler', function () {
-            return new Handler\DirectoryHandler(
-                $this->app->make('icr.config')
-            );
-        });
-
-        // Original file handler
-        $this->app->bind('icr.uploaded_file.handler', function () {
-            return new Handler\UploadedFileHandler(
-                $this->app->make('icr.config')
-            );
-        });
-
-        // Open image handler
-        $this->app->bind('icr.open_image.handler', function () {
-            return new Handler\OpenImageHandler(
-                $this->app->make('icr.config')
-            );
-        });
-
-        // Delete image handler
-        $this->app->bind('icr.delete_image.handler', function () {
-            return new Handler\DeleteImageHandler(
-                $this->app->make('icr.config')
-            );
-        });
-
-        // Directory tree reader
-        $this->app->bind('icr.directory_tree.reader', function () {
-            return new DirectoryTreeReader();
-        });
-
-        // ICR main processor class
-        $this->app->bind('icr.processor', function () {
-            return new Processor(
-                $this->app->make('icr.configuration_validation.handler'),
-                $this->app->make('icr.directory.handler'),
-                $this->app->make('icr.uploaded_file.handler'),
-                $this->app->make('icr.open_image.handler'),
-                $this->app->make('icr.delete_image.handler'),
-                $this->app->make('icr.directory_tree.reader')
-            );
-        });
-
-        // Rebuild command
-        $this->app->bind('icr.rebuild.command', function () {
-            return new RebuildCommand(
-                $this->app->make('icr.processor')
-            );
-        });
-
-        // Commands
-        $this->commands(['icr.rebuild.command']);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function provides()
-    {
-        return ['icr.rebuild.command'];
     }
 }
--- src/TableSorter.php
+++ src/TableSorter.php
@@ -1,12 +1,48 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 12.11.15
- * Time: 08:44
- */
+namespace Todstoychev\TableSorter;
+
 class TableSorter
 {
+    /**
+     * Table column sorter
+     *
+     * @param string $action "Controller@action"
+     * @param string $columnName Column name to show in the table
+     * @param string $param Parameter to order(database table column name)
+     * @param string $order Order direction
+     * @param integer $limit Items per page
+     *
+     * @return string
+     */
+    public static function sort($action, $columnName, $param, $order = 'asc', $limit = 10) {
+        If ($order == 'asc') {
+            $order = 'desc';
+        } else {
+            $order = 'asc';
+        }
 
+        return view('vendor.todstoychev.table-sorter.sort');
+    }
+
+    /**
+     * Table column sorter
+     *
+     * @param string $action "Controller@action"
+     * @param string $columnName Column name
+     * @param string $search Search keyword
+     * @param string $param Parameter to order(databese table column name)
+     * @param string $order Order direction
+     *
+     * @return string
+     */
+    public static function sortSearch($action, $columnName, $search = null, $param = null, $order = 'asc') {
+        If ($order == 'asc') {
+            $order = 'desc';
+        } else {
+            $order = 'asc';
+        }
+
+        return view('vendor.todstoychev.table-sorter.sort_search');
+    }
 }
